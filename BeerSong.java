0ff6e092a241d788e9f42aa45a8355c18c86f014
Marcel Jonas <mjonas@oakland.edu>
2019-02-10 22:23:21 -0500

    added a while loop to print the bottles of beer on the wall

--- BeerSong.java
+++ BeerSong.java
@@ -6,5 +6,13 @@ public class BeerSong {
 
 
     public static void Ninety_Nine_Bottles_of_Beer(){
+
+        int beerNumber = 99;
+        String word = "bottles";
+	while (beerNumber > 0){
+
+	    System.out.println(beerNumber + " " + word + " of beer on the wall, " + beerNumber + " " + word + " of beer");
+            beerNumber = beerNumber - 1;
+    }
     }
 }
