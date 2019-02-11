7a913bdaf5e0706e89d64c628aae536a0b8d545f
Marcel Jonas <mjonas@oakland.edu>
2019-02-10 22:14:03 -0500

    added the main and 99 bootles function

---
+++ BeerSong.java
@@ -0,0 +1,10 @@
+public class BeerSong {
+
+    public static void main(String[] args) {
+        Ninety_Nine_Bottles_of_Beer();
+    }
+
+
+    public static void Ninety_Nine_Bottles_of_Beer(){
+    }
+}
